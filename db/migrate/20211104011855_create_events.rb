df0ddd280918adb3f9488273d9fe8361cd31d557
EpicureanHeron <joethor1989@gmail.com>
2021-11-03 20:30:14 -0500

    models

---
+++ db/migrate/20211104011855_create_events.rb
@@ -0,0 +1,10 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string "First Name"
+      t.string "Last Name"
+      t.date "Date of Birth"
+      t.timestamps
+    end
+  end
+end
